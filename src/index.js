56c0d66459743961a563a78a9b846d13e620f7f6
Kiisi Felix Destiny <98293558+Kiisi-Felix-Destiny@users.noreply.github.com>
2022-07-24 00:13:35 +0100

    Done

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {HelmetProvider} from 'react-helmet-async'
+
 
 ReactDOM.render(
-    <App />,
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>,
   document.getElementById('root') 
 );
 
