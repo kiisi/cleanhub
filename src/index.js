9993321e6c89f160b7d4d2e87d4c15d8002dc28e
kiisi <kiisifelix06@gmail.com>
2022-09-17 13:00:22 +0100

    added preloader

--- src/index.js
+++ src/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
-import { hydrate, render } from "react-dom";
+import ReactDOM from "react-dom";
 import './index.css';
 import App from './App';
 
 
 const rootElement = document.getElementById("root");
 
-if (rootElement.hasChildNodes()){
-  hydrate(<App/>, rootElement)
-}
-else{
-  render(<App/>, rootElement)
-}
+ReactDOM.render(<App/>, rootElement)
+
 
 
