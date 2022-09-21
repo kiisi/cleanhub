8998bb304d86f576b05f545b33665709b22e8d6f
Victor Udoh <37489571+Victorujoshua@users.noreply.github.com>
2022-09-21 17:55:32 +0100

    Update Header.js

--- src/components/Main/Header/Header.js
+++ src/components/Main/Header/Header.js
@@ -8,8 +8,8 @@ const Header = () => {
     <header className="k-header">
       <div className="header-container">
         <div className="header-main">
-          <h1>Market place</h1>
-          <h1>for bespoke wears</h1>
+          <h1>Shop for bespoke wears</h1>
+          <h1>Right from ur smart phone</h1>
         </div>
         <div className="header-sub">
           <p>A marketplace for fashion designers in Africa to help meet the increasing demand for bespoke wears
