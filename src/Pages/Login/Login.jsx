6406edbcb86a64c523ff1d58a2826edeb8f67237
kiisi <kiisifelix06@gmail.com>
2022-11-17 00:54:31 +0100

    login page updates 70% done

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -4,7 +4,7 @@ import login from '../../assets/login_img.PNG'
 import './Login.scss'
 import Input from '../../components/Input/Input'
 import Button from '../../components/Button/Button'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 const Login = () => {
 
     const [email, setEmail] = useState('')
@@ -15,33 +15,34 @@ const Login = () => {
 
     return (
 
-            <div className="max-wrapper">
-                <div className="max-wrapper-content">
-                    <Navbar />
+        <div className="max-wrapper">
+            <div className="max-wrapper-content">
+                <Navbar />
+            </div>
+            <div className="login">
+                <div className="login-img">
+                    <img src={login} alt="login" />
                 </div>
-                <div className="login">
-                    <div className="login-img">
-                        <img src={login} alt="login" />
-                    </div>
-                    <div className="login-content">
-                        <div className="max-wrapper-content">
-                            <div className="login-content-max_width">
-                                <h1 className="heading-lg">Welcome Back</h1>
-                                <div className="login-content-row">
-                                    <Input type="email" labelText="Email" value={email} setValue={(e) => setEmail(e.target.value)} />
-                                </div>
-                                <div className="login-content-row">
-                                    <Input type="password" labelText="Password" value={password} setValue={(e) => setPassword(e.target.value)} />
-                                </div>
+                <div className="login-content">
+                    <div className="max-wrapper-content">
+                        <div className="login-content-max_width">
+                            <h1 className="heading-lg">Welcome Back</h1>
+                            <div className="login-content-row">
+                                <Input type="email" labelText="Email" value={email} setValue={(e) => setEmail(e.target.value)} />
+                            </div>
+                            <div className="login-content-row">
+                                <Input type="password" labelText="Password" value={password} setValue={(e) => setPassword(e.target.value)} />
+                            </div>
 
-                                <div className="login-content-password_service">
-                                    <div className="btn-sm login-content-password_service-btn_s">SIGN IN</div>
-                                    <div className="login-content-password_service-remember_me">
-                                        <span>Remember me</span>
-                                        <Button />
-                                    </div>
+                            <div className="login-content-password_service">
+                                <div className="btn-sm login-content-password_service-btn_s">SIGN IN</div>
+                                <div className="login-content-password_service-remember_me">
+                                    <span>Remember me</span>
+                                    <Button />
                                 </div>
-                                <div className="login-content-providers">
+                            </div>
+                            <div className="login-content-providers">
+                                <div className="login-content-providers-btn">
                                     <div>OR</div>
                                     <div className="login-content-providers-btn_g">
                                         <div>
@@ -55,14 +56,16 @@ const Login = () => {
                                     </div>
                                     <div className="login-content-providers-btn_f">FACEBOOK</div>
                                 </div>
-                                <div className="login-content-getstarted">
-                                    <span>New to Cleanhub? </span><Link to="/signup">Create account</Link>
-                                </div>
+                                <Link to="/forgotpassword" className="login-content-providers-forgot_psw"><span>Forgot Password?</span></Link>
+                            </div>
+                            <div className="login-content-getstarted">
+                                <span>New to Cleanhub? </span><Link to="/signup">Create account</Link>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
     )
 }
 
