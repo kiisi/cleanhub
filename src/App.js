cd5871b4b74e2008b1dd1a6160f106a17457e401
kiisi <kiisifelix06@gmail.com>
2022-11-17 00:21:47 +0100

    Update - scroll to top

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Footer from './components/Footer/Footer';
 import Home from './Pages/Home/Home'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './Pages/Login/Login';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 
 const App = () => {
 
 	return (
 		<Router>
-			<div className="app">
-				<Routes>
-					<Route exact path="/" element={<Home/>}/>
-					<Route exact path="/signin" element={<Login/>}/>
-				</Routes>
-				<Footer />
-			</div>
+			<ScrollToTop>
+				<div className="app">
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/signin" element={<Login />} />
+					</Routes>
+					<Footer />
+				</div>
+			</ScrollToTop>
 		</Router>
 	);
 }
