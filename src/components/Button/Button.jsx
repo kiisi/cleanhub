80daecdeb179c1180b3455bc2a2968401fbc5af4
kiisi <kiisifelix06@gmail.com>
2022-11-17 03:03:05 +0100

    Changes on toggle btn

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -11,7 +11,7 @@ const Button = () => {
     return (
         <div className="toggle-btn">
             <div onClick={toggleHandler} className={click ? "toggle-btn--circle toggle" : "toggle-btn--circle"}></div>
-            <div className="toggle-btn--slider"></div>
+            <div className={click ? "toggle-btn--slider toggle" : "toggle-btn--slider"}></div>
         </div>
     )
 }
